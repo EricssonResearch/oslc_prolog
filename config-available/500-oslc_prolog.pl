e9c30468bccb45ac8f545daa572b3d1232a889c2
Leonid Mokrushin <likelion@gmail.com>
2017-10-31 12:51:38 +0100

    Set CPACK loading priority (#55)
    * set the CPACK loading priority
    
    * fixed computation of shape applicability to types

--- config-available/500-oslc_prolog.pl
+++ config-available/500-oslc_prolog.pl
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-:- module(conf_oslc_prolog, []).
+:- module('500-oslc_prolog', []).
 
 /** <module> OSLC Prolog
 */
