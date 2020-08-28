4def80d0627db16eca373d1b3a6098cf19731ec2
Leonid Mokrushin <leonid.mokrushin@ericsson.com>
2020-08-28 12:58:38 +0200

    polishing interface

--- lib/oslc_dict2.pl
+++ lib/oslc_dict2.pl
@@ -14,11 +14,13 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-:- module(oslc_dict2, [ graph_dict/3 ]).
+:- module(oslc_dict2, [ graph_dict/3,
+                        resource_dict/3 ]).
 
 :- use_module(library(semweb/rdf11)).
 
-:- predicate_options(graph_dict/3, 3, [ skip(list),
+:- predicate_options(graph_dict/3, 3, [ graph(atom),
+                                        skip(list),
                                         lists(boolean),
                                         subject(callable),
                                         property(callable),
@@ -27,14 +29,20 @@ limitations under the License.
                                         name(boolean)
                                       ]).
 
-:- meta_predicate graph_dict(-, -, :).
+:- meta_predicate graph_dict(-, -, :),
+                  resource_dict(-, -, :).
 
 :- rdf_meta graph_dict(r, -, t),
-            read_nested_list(r, -, -, -, -, -).
+            resource_dict(r, -, t),
+            read_nested_list(r, -, -, -, -).
 
 graph_dict(Graph, Dict, Options) :-
   meta_options(is_meta, Options, Options1),
-  read_graph_forest(Graph, Dict, Options1),
+  ( select(graph(_), Options1, Options2)
+  -> true
+  ; Options2 = Options1
+  ),
+  read_graph_forest(Dict, [graph(Graph)|Options2]),
   name_multireferenced_bnodes(Dict).
 
 is_meta(subject).
@@ -65,7 +73,8 @@ name_bnodes([H|T], Num) :-
   NextNum is Num + 1,
   name_bnodes(T, NextNum).
 
-read_graph_forest(Graph, Forest, Options) :-
+read_graph_forest(Forest, Options) :-
+  option(graph(Graph), Options, _),
   findall(Subject, (
     ( rdf(Subject, _, _, Graph),
       \+ ( % TODO: also handle situation with dangling cyclic bnodes
@@ -80,14 +89,20 @@ read_graph_forest(Graph, Forest, Options) :-
   ), SubjectList),
   sort(SubjectList, Subjects),
   empty_assoc(E),
-  read_graph_forest_(Subjects, Graph, [], Forest, E, _, Options).
+  read_graph_forest_(Subjects, [], Forest, E, _, Options).
 
-read_graph_forest_([], _, Forest, Forest, Seen, Seen, _) :- !.
-read_graph_forest_([H|T], Graph, Forest0, Forest, Seen0, Seen, Options) :-
-  read_resource_tree(H, Graph, Tree, Seen0, Seen1, Options),
-  read_graph_forest_(T, Graph, [Tree|Forest0], Forest, Seen1, Seen, Options).
+read_graph_forest_([], Forest, Forest, Seen, Seen, _) :- !.
+read_graph_forest_([H|T], Forest0, Forest, Seen0, Seen, Options) :-
+  read_resource_tree(H, Tree, Seen0, Seen1, Options),
+  read_graph_forest_(T, [Tree|Forest0], Forest, Seen1, Seen, Options).
 
-read_resource_tree(S, Graph, Tree, Seen0, Seen, Options) :-
+resource_dict(Resource, Dict, Options) :-
+  meta_options(is_meta, Options, Options1),
+  empty_assoc(E),
+  read_resource_tree(Resource, Dict, E, _, Options1).
+
+read_resource_tree(S, Tree, Seen0, Seen, Options) :-
+  option(graph(Graph), Options, _),
   findall(P-O, (
     ( var(Graph)
     -> rdf(S, P, O)
@@ -109,28 +124,28 @@ read_resource_tree(S, Graph, Tree, Seen0, Seen, Options) :-
   ),
   put_assoc(S, Seen0, SV, Seen1),
   dict_create(Tree0, SV, []),
-  read_resource_tree_(S, POs, Graph, Tree0, Tree, Seen1, Seen, Options).
+  read_resource_tree_(S, POs, Tree0, Tree, Seen1, Seen, Options).
 
-read_resource_tree_(S, [], _, Tree, Tree, Seen0, Seen, _) :- !,
+read_resource_tree_(S, [], Tree, Tree, Seen0, Seen, _) :- !,
   ( rdf_is_bnode(S),
     get_assoc(S, Seen0, _)
   -> put_assoc(S, Seen0, Tree, Seen)
   ; Seen = Seen0
   ).
 
-read_resource_tree_(S, [P-O|T], Graph, Tree0, Tree, Seen0, Seen, Options) :-
+read_resource_tree_(S, [P-O|T], Tree0, Tree, Seen0, Seen, Options) :-
   option(property(Callback), Options),
   apply(Callback, [S, P, O, Key, Value, Options]), !,
   add_predicate_value(Key, Value, Tree0, Tree1),
-  read_resource_tree_(S, T, Graph, Tree1, Tree, Seen0, Seen, Options).
+  read_resource_tree_(S, T, Tree1, Tree, Seen0, Seen, Options).
 
-read_resource_tree_(S, [P-L|T], Graph, Tree0, Tree, Seen0, Seen, Options) :-
+read_resource_tree_(S, [P-L|T], Tree0, Tree, Seen0, Seen, Options) :-
   read_literal(L, O), !,
   resource_key(P, PV, Options),
   add_predicate_value(PV, O, Tree0, Tree1),
-  read_resource_tree_(S, T, Graph, Tree1, Tree, Seen0, Seen, Options).
+  read_resource_tree_(S, T, Tree1, Tree, Seen0, Seen, Options).
 
-read_resource_tree_(S, [P-O|T], Graph, Tree0, Tree, Seen0, Seen, Options) :-
+read_resource_tree_(S, [P-O|T], Tree0, Tree, Seen0, Seen, Options) :-
   ( get_assoc(O, Seen0, OV)
   -> resource_key(P, PV, Options),
      add_predicate_value(PV, OV, Tree0, Tree1),
@@ -139,11 +154,11 @@ read_resource_tree_(S, [P-O|T], Graph, Tree0, Tree, Seen0, Seen, Options) :-
     ( option(lists(Lists), Options, true),
       Lists == true,
       rdf_list(O)
-    -> read_nested_list(O, OV, Graph, Seen1, Seen2, Options),
+    -> read_nested_list(O, OV, Seen1, Seen2, Options),
        resource_key(P, PV, Options),
        add_predicate_value(PV, OV, Tree0, Tree1)
     ; rdf_is_bnode(O)
-    -> read_resource_tree(O, Graph, OV, Seen1, Seen2, Options),
+    -> read_resource_tree(O, OV, Seen1, Seen2, Options),
        resource_key(P, PV, Options),
        add_predicate_value(PV, OV, Tree0, Tree1)
     ; Seen2 = Seen1,
@@ -152,13 +167,14 @@ read_resource_tree_(S, [P-O|T], Graph, Tree0, Tree, Seen0, Seen, Options) :-
       add_predicate_value(PV, OV, Tree0, Tree1)
     )
   ),
-  read_resource_tree_(S, T, Graph, Tree1, Tree, Seen2, Seen, Options).
+  read_resource_tree_(S, T, Tree1, Tree, Seen2, Seen, Options).
 
 read_literal(^^(L, _), L).
 read_literal(@(L, _), L).
 
-read_nested_list(rdf:nil, [], _, Seen, Seen, _).
-read_nested_list(RDFList, [H|T], Graph, Seen0, Seen, Options) :-
+read_nested_list(rdf:nil, [], Seen, Seen, _).
+read_nested_list(RDFList, [H|T], Seen0, Seen, Options) :-
+  option(graph(Graph), Options, _),
   ( ( var(Graph)
     -> rdf(RDFList, rdf:first, F),
        rdf(RDFList, rdf:rest, R)
@@ -167,15 +183,15 @@ read_nested_list(RDFList, [H|T], Graph, Seen0, Seen, Options) :-
     )
   -> ( read_literal(F, H)
      -> Seen1 = Seen0
-     ; ( read_nested_list(F, H, Graph, Seen0, Seen1, Options)
+     ; ( read_nested_list(F, H, Seen0, Seen1, Options)
        -> true
        ; ( get_assoc(F, Seen0, H)
          -> Seen1 = Seen0
-         ; read_resource_tree(F, Graph, H, Seen0, Seen1, Options)
+         ; read_resource_tree(F, H, Seen0, Seen1, Options)
          )
        )
      ),
-     read_nested_list(R, T, Graph, Seen1, Seen, Options)
+     read_nested_list(R, T, Seen1, Seen, Options)
   ).
 
 add_predicate_value(P, O, Tree0, Tree) :-
